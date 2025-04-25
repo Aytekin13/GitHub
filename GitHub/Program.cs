505a375b5ce1ca864fe6a6226d50e31c346a44f0
Aytekin13 <mrsyt@gmail.com>
2025-04-26 01:21:50 +0400

    commit

--- GitHub/Program.cs
+++ GitHub/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Runtime.Intrinsics.X86;
 using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace GitHub_1
 {
@@ -297,29 +298,254 @@ namespace GitHub_1
             #endregion
 
             #region TASK7
-           /* Console.WriteLine("Həftənin gününü daxil edin");
-            int heftenin_gunu = Convert.ToInt16(Console.ReadLine());
-            switch(heftenin_gunu)
+            /* Console.WriteLine("Həftənin gününü daxil edin");
+             int heftenin_gunu = Convert.ToInt16(Console.ReadLine());
+             switch(heftenin_gunu)
+             {
+                 case 1:
+                 case 2:
+                 case 3:
+                 case 4:
+                 case 5:
+                     Console.WriteLine("İş günü");
+                     break;
+                 case 6:
+                 case 7:
+                     Console.WriteLine("Həftəsonu");
+                     break;
+                 default:
+                     Console.WriteLine("Düzgün gün daxil edin");
+                     break;
+             }*/
+            #endregion
+
+            //Loops
+            #region TASK1
+            /*int sagird_sayi=Convert.ToInt32(Console.ReadLine());
+            int a;
+            for(int i=1;i<=sagird_sayi;i++)
             {
-                case 1:
-                case 2:
-                case 3:
-                case 4:
-                case 5:
-                    Console.WriteLine("İş günü");
-                    break;
-                case 6:
-                case 7:
-                    Console.WriteLine("Həftəsonu");
-                    break;
-                default:
-                    Console.WriteLine("Düzgün gün daxil edin");
-                    break;
+                a = i;
+                Console.Write(a+ ",");
             }*/
             #endregion
 
+            #region TASK2
+            /*int N=int.Parse(Console.ReadLine());
+            int cem=0;
+            for (int i = 0; i<=N; i++)
+            {
+                if (i % 2 ==1)
+                { cem =cem + i;}
+            }
+            Console.WriteLine("Tek ededlerin cemi="+cem);*/
+            #endregion
 
+            #region TASK3
+            /*Console.WriteLine("Eded daxil edin");
+            int s=int.Parse(Console.ReadLine());
+            Console.WriteLine("Ededin reqemlerinin sayi");
+            int n=int.Parse(Console.ReadLine());
+            if (n == 1)
+            {
+                for (int i = 0; i <= 9; i++)
+                { 
+                    
+                    if (s == i)
+                    { Console.WriteLine(i); }
 
+                }
+            }
+            else if (n == 2)
+            {
+                for (int i = 10; i <= 99; i++)
+                {
+                    int eded = i;
+                    int cem = 0;
+                    while (eded > 0)
+                    {
+                        cem += eded % 10;
+                        eded /= 10;
+                    }
+                    if (cem==s)
+                    { Console.WriteLine(i); }
+
+                }
+            }
+            else if (n == 3)
+            {
+                for (int i = 100; i <= 999; i++)
+                {
+                    int eded = i;
+                    int cem = 0;
+                    while (eded > 0)
+                    {
+                        cem += eded % 10;
+                        eded /= 10;
+                    }
+                    if (cem == s)
+                    { Console.WriteLine(i); }
+
+                }
+            }
+            else if (n == 4)
+            {
+                for (int i = 1000; i <= 9999; i++)
+                {
+                    int eded = i;
+                    int cem = 0;
+                    while (eded > 0)
+                    {
+                        cem += eded % 10;
+                        eded /= 10;
+                    }
+                    if (cem == s)
+                    { Console.WriteLine(i); }
+
+                }
+            }
+            else if (n == 5)
+            {
+                for (int i = 10000; i <= 99999; i++)
+                {
+                    int eded = i;
+                    int cem = 0;
+                    while (eded > 0)
+                    {
+                        cem += eded % 10;
+                        eded /= 10;
+                    }
+                    if (cem == s)
+                    { Console.WriteLine(i); }
+
+                }
+            }
+            else if (n == 6)
+            {
+                for (int i = 100000; i <= 999999; i++)
+                {
+                    int eded = i;
+                    int cem = 0;
+                    while (eded > 0)
+                    {
+                        cem += eded % 10;
+                        eded /= 10;
+                    }
+                    if (cem == s)
+                    { Console.WriteLine(i); }
+
+                }
+            }*/
+            #endregion
+
+            #region TASK4
+            /*Console.WriteLine("Birinci ededi daxil edin");
+            int num1 = Convert.ToInt32((Console.ReadLine()));
+            Console.WriteLine("Ikinci ededi daxil edin");
+            int num2 = Convert.ToInt32((Console.ReadLine()));
+            double a, b;
+            for(int i = num1; i <= num2; i++)
+            {
+                a = Math.Pow(i,2);
+                b = Math.Pow(i, 3);
+                Console.WriteLine("Eded   "+i+"  "+"Ededin kvadrati   "+a+"   "+"Ededin kubu   "+b);
+            }*/
+            #endregion
+
+            #region TASK5
+            /*int n=int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            double a;
+            for(int i=1; i <= n; i++)
+            {
+                a = Math.Pow(i, 2);
+                if (a < n)
+                Console.WriteLine(a);
+            }*/
+
+            #endregion
+
+            #region TASK6
+            /*Console.WriteLine("Eded daxil edin");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Reqemi daxil edin");
+            int a=int.Parse(Console.ReadLine());
+            int say = 0,b;
+           
+            while (n >0)
+            {
+                b = n % 10;
+                n = n / 10;
+                if (b == a)
+                { say = say + 1; }
+            }
+            Console.WriteLine("Reqemin sayi   " + say);*/
+            #endregion
+
+            #region TASK7
+            /* Console.WriteLine("Birinci ededi daxil edin");
+             int a=Convert.ToInt32(Console.ReadLine());
+             Console.WriteLine("Ikinci ededi daxil edin");
+             int b = Convert.ToInt32(Console.ReadLine());
+             int cem=0;
+             for (int i = a; i <= b; i++)
+             {
+                 cem = cem + i;
+             }
+             Console.WriteLine("Cem   " + cem);*/
+            #endregion
+
+            #region TASK8
+            /* Console.Write("n ededini daxil edin: ");
+             int n = int.Parse(Console.ReadLine());
+             int say = 0;
+
+             for (int i = 1; i <= n; i++)
+             {
+                 if (n % i == 0)
+                 {
+                     say++;
+                 }
+             }
+             Console.WriteLine("Ədədin bölənlərinin sayı: " + say);*/
+            #endregion
+
+            #region TASK9
+            /*Console.Write("n ededini daxil edin: ");
+            int n = int.Parse(Console.ReadLine());
+            int say = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0 && i%2==1)
+                {
+                    say++;
+                    Console.WriteLine(i);
+                }
+                
+            }
+            Console.WriteLine("Ədədin bölənlərinin sayı: " + say);*/
+            #endregion
+
+            #region TASK10
+            /*Console.WriteLine("Ededi daxil edin");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ededlerin siyahisi:");
+            for (int i = 100; i <= 999; i++)
+            {
+                int eded = i;
+                int cem = 0;
+                while (eded > 0)
+                {
+                    cem += eded % 10;
+                    eded /= 10;
+                }
+                if (cem == n)
+                { Console.WriteLine(i); }
+
+            }*/
+
+            #endregion
 
         }
     }
